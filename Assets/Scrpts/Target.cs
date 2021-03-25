51058eacb9faef73eb9b75c2a6299c1c30fc97cb
jhed-98 <cod.mobile.jhed@gmail.com>
2021-03-25 16:12:04 -0500

    first commit

---
+++ Assets/Scrpts/Target.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameManager gameManager;
+    private AudioSource _audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager= 
+            GameObject.Find("EventSystem").GetComponent<GameManager>();
+        Destroy(gameObject, 2f);
+    }
+
+    void OnMouseDown()
+    {
+        gameManager.IncrementScore();
+        Destroy(gameObject);
+    }
+    void Awake(){
+        _audio=GetComponent<AudioSource>();
+    }
+
+    void Destroy(){
+        _audio.Play();
+    }
+
+}
---
+++ Assets/Scrpts/disparar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparar : MonoBehaviour
+{
+    float tiempo  = 0.0f;
+    public AudioClip sonidoClip;
+    public AudioSource _audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+     _audio = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0)){
+            _audio.PlayOneShot(sonidoClip);
+        }
+    }
+}
